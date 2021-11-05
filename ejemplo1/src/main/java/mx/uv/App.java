77054c21b6e79b1b6df728519a08b9c70c214931
LuisMendozaSanchez <tavo.tpc@gmail.com>
2021-11-05 12:48:40 -0600

    Proyecto maven básico con integración de spark java

--- ejemplo1/src/main/java/mx/uv/App.java
+++ ejemplo1/src/main/java/mx/uv/App.java
@@ -1,4 +1,5 @@
 package mx.uv;
+import static spark.Spark.*;
 
 /**
  * Hello world!
@@ -9,5 +10,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        get("/hello", (a, b) -> "Hola amigos");
     }
 }
